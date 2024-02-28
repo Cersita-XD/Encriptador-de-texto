ca2539e49590a519b66a943886a6cfb654194162
Cersita-XD <cesarperozoetir@gmail.com>
2024-02-28 12:26:36 -0400

    Ajustes en colores de botones y habilitacion de la salida

--- back.js
+++ back.js
@@ -40,6 +40,8 @@ function encrip() {
     document.querySelector('.btn-encriptar').disabled = true;
     document.querySelector('.btn-desencriptar').disabled = false;
     document.querySelector('.btn-copiar').disabled = false;
+    document.getElementById("texto").disabled=true;
+
 }
 //FUNCION PARA LA DESENCRIPTACION DEL TEXTO//
 function reverse() {
@@ -72,6 +74,8 @@ function reverse() {
     document.querySelector('.btn-encriptar').disabled = false;
     document.querySelector('.btn-desencriptar').disabled = true;
     document.querySelector('.btn-copiar').disabled = true;
+    document.getElementById("texto").disabled=false;
+
 }
 //FUNCION PARA EL COPIADO EN EL PORTAPAPELES//
 function copiar() {
